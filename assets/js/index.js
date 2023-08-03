73af10e37f5aa5296c68bbfa9f1f388078ff1290
Behnaz <behnaz.pourafshari@ft.com>
2023-08-03 10:59:43 +0100

    shuffle cards

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+const cards = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 16, 15, 14, 13, 12, 11,
+  10, 9, 8, 7, 6, 5, 4, 3, 2, 1,
+];
+let suffledCards = cards
+  .map((element) => ({ sort: Math.random(), value: element }))
+  .sort((a, b) => a.sort - b.sort)
+  .map((element) => element.value);
+console.log(suffledCards);
