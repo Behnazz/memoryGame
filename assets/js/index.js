99d5edc5b5f82e96e65a2ee1b567a04dd4684e7d
Behnaz <behnaz.pourafshari@ft.com>
2023-08-07 10:18:08 +0100

    make cards with javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+//Fisher-Yates shuffle algorithm
 const cards = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 16, 15, 14, 13, 12, 11,
   10, 9, 8, 7, 6, 5, 4, 3, 2, 1,
@@ -6,4 +7,18 @@ let suffledCards = cards
   .map((element) => ({ sort: Math.random(), value: element }))
   .sort((a, b) => a.sort - b.sort)
   .map((element) => element.value);
-console.log(suffledCards);
+5;
+
+const gameBoard = document.getElementById('gameBoard'); //create game board element
+
+//create cards
+function createCards() {
+  for (let i = 0; i < suffledCards.length; i++) {
+    const card = document.createElement('div'); //make a div
+    card.classList.add('card'); //add class
+    card.setAttribute('data-card', suffledCards[i]); //add data attribute
+    card.textContent = suffledCards[i]; //add text
+    gameBoard.appendChild(card); //append to game board
+  }
+}
+createCards();
