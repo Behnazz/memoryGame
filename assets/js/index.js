57350366c393189d09a64116a7d1e99bb75e7d57
Behnaz <behnaz.pourafshari@ft.com>
2023-08-21 11:49:24 +0100

    fix the bug for aria-attribute

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,7 @@ const cards = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 16, 15, 14, 13, 12, 11,
   10, 9, 8, 7, 6, 5, 4, 3, 2, 1,
 ];
-let suffledCards = cards
+let shuffledCards = cards
   .map((element) => ({ sort: Math.random(), value: element }))
   .sort((a, b) => a.sort - b.sort)
   .map((element) => element.value);
@@ -17,14 +17,17 @@ let flippedCards = []; //create array for flipped cards
 const gameBoard = document.getElementById('gameBoard'); //create game board element
 //create cards
 function createCards() {
-  for (let i = 0; i < suffledCards.length; i++) {
+  for (let i = 0; i < shuffledCards.length; i++) {
     const card = document.createElement('div'); //make a div
     card.classList.add('card'); //add class
-    card.setAttribute('data-card', suffledCards[i]); //add data attribute
-    card.textContent = suffledCards[i]; //add text
+    card.setAttribute('data-card', shuffledCards[i]); //add data attribute
+    card.textContent = shuffledCards[i]; //add text
+    card.setAttribute('role', 'option'); //add ARIA role for each card
+    card.setAttribute('aria-hidden', 'false');
+    card.setAttribute('tabindex', '0'); // tab key to cycle through the cards
     gameBoard.appendChild(card); //append to game board
 
-    gameBoard.addEventListener('click', flipCard); //add event listener
+    card.addEventListener('click', flipCard); //add event listener
   }
 }
 
@@ -45,11 +48,13 @@ function flipCard(event) {
         //update the flippedCards array to deselect the same card
         if (index !== -1) {
           flippedCards.splice(index, 1);
+          clickedCard.setAttribute('aria-selected', 'false');
         }
       } else {
         // If the card is not flipped, mark it as flipped
         clickedCard.classList.add('flipped');
         flippedCards.push(clickedCard);
+        clickedCard.setAttribute('aria-selected', 'true');
         if (flippedCards.length === 2) {
           checkMatch();
         }
@@ -83,8 +88,13 @@ function checkMatch() {
     card1.classList.add('notMatched');
     card2.classList.add('notMatched');
     setTimeout(() => {
+      card1.setAttribute('aria-selected', 'false');
+      card2.setAttribute('aria-selected', 'false');
+      card1.setAttribute('aria-hidden', 'false');
+      card2.setAttribute('aria-hidden', 'false');
       card1.classList.remove('flipped', 'notMatched');
       card2.classList.remove('flipped', 'notMatched');
+
       flippedCards = []; // after a comparison it should be cleared for the next comparison
     }, 1000);
   }
@@ -97,7 +107,7 @@ function showWinningMessage() {
   winningMessage.showModal();
 
   //paly again button
-  constPlayAgainButton = document.getElementById('playAgain');
+  const PlayAgainButton = document.getElementById('playAgain');
   playAgain.addEventListener('click', () => {
     winningMessage.close();
     //reset game
